4e2ca017505866dd3b98e764b962057e1f53339d
Lena Pan <lenapan.tech@gmail.com>
2021-10-11 22:16:19 -0700

    redo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
-// import React, { Component } from "react";
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoPractice = require('./mongo');
+const mongo2 = require('./mongo2');
 
-// import Home from "./pages/Home";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+const app = express();
 
-import "./styles/style.css"
-// import { Routes } from "react-router";
+app.use(bodyParser.json());
 
-  function App() {
+//MUST USE TO PREVENT CORS ERROR
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Content-Type, Accept, Authorization, *');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+})
+app.post('/api/Comments', mongoPractice.ADD);
+app.post('/api/Titles', mongo2.addTitles);
 
-    // const { movieItems } = data;
-    // const [cartItems, setCartItems] = useState([]);
+app.get('/api/Comments', mongoPractice.getComments);
+app.get('/api/Titles', mongo2.retrieveTitles);
 
-    return (
-      <div>
-          {/* <Routes> */}
-            
-              <Header />
-              <Main/> 
-              <Footer year={new Date().getFullYear()}/>
-              
-                  {/* <Route exact path="/" component={Home} /> */}
-        
-      </div>
-      );
-   }
-
-export default App;
+app.listen(5000 || process.env.PORT); //Heroku provides port
